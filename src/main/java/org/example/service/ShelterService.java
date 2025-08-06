0f07a6e4f1c0c0e320cc280e92ae5fc0f221d683
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-08-06 11:44:28 +0300

    remove import

--- src/main/java/org/example/service/ShelterService.java
+++ src/main/java/org/example/service/ShelterService.java
@@ -7,7 +7,6 @@ import org.example.util.Commands;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ShelterService {
